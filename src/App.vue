d8b989f43e6966ce65539953c7877576104d6e92
Eminence <mulemanowa@gmail.com>
2021-11-15 09:45:26 +0100

    open sans font added

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:wght@300;400;500;600;700;800&display=swap');
 
 
 </style>
