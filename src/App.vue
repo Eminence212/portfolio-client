6af5ca0453d08f3191226412916889edb4a10bd0
Eminence <mulemanowa@gmail.com>
2021-11-15 17:06:39 +0100

    footer feature added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <Header />
+    <!-- <Header /> -->
+    <Footer/>
   </div>
 </template>
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 import { select, scrollto } from "./utils/Functions";
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
   created() {},
 };
